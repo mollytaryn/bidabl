41710fcd43f4eb470676fcc2511a99ac473a9eaf
mollytaryn <mollytaryn@gmail.com>
2015-06-16 11:00:10 -0500

    setup root route and user sign in /sign up routes

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
+# gem 'haml-rails'
 
 gem 'jquery-rails'
 gem 'turbolinks'
@@ -27,13 +28,13 @@ group :development, :test do
 
   gem 'fabrication'
   gem 'faker'
-  gem 'launchy'
   gem 'rspec-rails'
-  gem 'shoulda-matchers'
 end
 
 group :test do
   gem 'capybara'
   gem 'database_cleaner'
+  gem 'launchy'
+  gem 'shoulda-matchers'
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :email, :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :username, length: {minimum: 2}
+  validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
+  validates :password, confirmation: true
+  validates :password, :password_confirmation, presence: { on: :create }
+
 end
