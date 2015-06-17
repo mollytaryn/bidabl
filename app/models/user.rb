33ccaa7f30c0d41cca3d160b52f9fbc2c2f91a19
mollytaryn <mollytaryn@gmail.com>
2015-06-17 13:39:08 -0500

    Sign up modal now working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :email, :name, presence: true
-  validates :name, length: { minimum: 2 }
+  validates :email, :username, presence: true
+  validates :name, length: { minimum: 2, allow_blank: true }
   validates :username, length: {minimum: 2}
   validates :email, format: { with: /.+@.+\..+/, message: "must be an email address" }, uniqueness: true
   validates :password, confirmation: true
