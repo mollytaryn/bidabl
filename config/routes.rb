a50c1d867e44206b4e610ceda8d5d064ed095bd8
mollytaryn <mollytaryn@gmail.com>
2015-06-23 17:15:43 -0500

    added ability for user to link to pinterest and updated logic for only showing edit and delete tools to the logged in user and profile areas that are in use

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :projects, only: [:new, :create, :show, :destroy] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 
   resources :categories, only: [:index, :show, :destroy]
